cfedfd6a3bd6b9b05a305f6e9133b7f917ed6932
zahra farokhi <z.farokhi63@gmail.com>
2021-04-19 12:57:29 +0430

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+require('./bootstrap');
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById('demo-sunmeno').innerHTML = "" + hours +
+        ":" + minutes + ":" + seconds + "";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo-sunmeno").innerHTML = "EXPIRED";
+    }
+}, 1000);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+            <script src="/js/index.js"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="/css/style.css" >
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.2/assets/owl.carousel.min.css" />
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.2/owl.carousel.min.js"></script>
+       
+
+    </head>
+    <body class="antialiased">
+     
+        <div class="bg">
+        <header class="headerr" id="mmy">
+            <div class="headerright">
+                <a class="logo"></a>
+                <input type="search" class="searchbox" placeholder="جستجو در دیجی‌کالا …">
+            </div>
+            <div class="headerleft">
+                <div class="box-user">
+                    <a class="box-left-user text-muted"> <i style="font-size:24px" class="fa">&#xf2c0;</i>ورود به حساب کاربری</a>
+                </div>
+                <a class="text-muted hyy">
+                    <i style="font-size:24px" class="fa icon-user-st">&#xf07a;</i>
+                </a>
+            </div>
+        </header>
+        <!-- //meno -->
+        <div>
+            <div class="meno" id="mynav">
+                <div class="headerright">
+                    <ul class="meno-headerr">
+
+                        <li> <a><i style="font-size:24px" class="fa ml-2">&#xf039;</i>دسته بندی کالا</li></a>
+                            <li class="">
+                                <div class="dropdown"><a>سوپرمارکت</a>
+                                    
+                                </div>
+                            </li>
+                            <li>
+                                <div class="dropdown">
+                                    <a>تخفیف ها و پیشنهاد ها</a>
+                                    <div class="sum-menmo">
+        <div class="drop-right">
+            <div>
+
+                <ul class="list-submeno">
+                    <li> <a>مشاهده همه تخفیف ها و پیشنهاد ها<i style="font-size:14px" class="fa text-muted mr-2">&#xf104;</i></a>
+                    </li>
+                    <li> <a>کالاهای شگفت انگیز<i style="font-size:14px" class="fa text-muted mr-2">&#xf104;</i></a>
+                    </li>
+                    <li> <a>شگفت انگیز سوپر مارکتی<i style="font-size:14px" class="fa text-muted mr-2">&#xf104;</i></a>
+                    </li>
+                    <li>
+                        <a>فروش ویژه<i style="font-size:14px" class="fa text-muted mr-2">&#xf104;</i></a>
+                    </li>
+                    <li class="text-muted"><a>کالا های دیجیتال</a></li>
+                    <li class="text-muted"> ابزار تجهیزات صنعتی خودرو </li>
+                    <li class="text-muted"><a>مد پوشاک</a></li>
+                    <li class="text-muted"><a>اسباب بازی، کودک و نوزاد</a></li>
+                    <li class="text-muted"><a>کالاهای سوپرمارکتی</a> </li>
+                    <li class="text-muted"><a>زیبایی و سلامت</a> </li>
+                    <li class="text-muted">
+                        <a>خانه و آشپزخانه</a>
+                    </li>
+                    <li class="text-muted"> <a>کتاب و لوازم  تحریر</a> </li>
+                    <li class="text-muted"> <a>ورزش و سفر</a> </li>
+                </ul>
+
+            </div>
+        </div>
+
+        <div class="droop-left">
+            <ul class="list-drop-left">
+                <li><a>چتر کم تر از 150 هزار تومان </a></li>
+                <li><a>کتاب چاپی تا 70%تخفیف</a></li>
+                <hr style="width: 90%;">
+                <li><a><i style="font-size:20px" class="fa text-muted ml-2">&#xf06b;</i>پر فروش ترین کالاها</a></li>
+                <li><a><i style="font-size:20px" class="fa text-muted ml-2">&#xf06b;</i>با هر خرید هدیه بگیرید!</a></li>
+                <li><a><i style="font-size:20px" class="fa text-muted ml-2">&#xf06b;</i>تخفیف پایان سال</a></li>
+                <li><a><i style="font-size:20px" class="fa text-muted ml-2">&#xf06b;</i>کارت هدیه خرید </a></li>
+                <li><a><i style="font-size:20px" class="fa text-muted ml-2">&#xf06b;</i>تازه های فروشنده جدید </a></li>
+            </ul>
+            <div class="img-off">
+                <a><img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/560de61ce10e86ee53603192ed6e3320fd0d0923_1610191059.png?x-oss-process=image/quality,q_80"></a>
+            </div>
+        </div>
+    </div>
+                                </div>
+                        </li>
+                            <li><a>دیجی کالای من</a></li>
+                            <li>
+                                <div class="dropdown">
+                                    <a>دیجی پلاس</a>
+                                    <div class="sum-meno">
+                                        <div class="drop-right">
+                                            <div>
+                                                <div class="c-dp-header-submenu__head-title ">
+                                                    خدمات ویژه کاربران <img src="https://www.digikala.com/static/files/4a2895fc.svg" alt="Digiplus"> </div>
+                                                <div class="c-dp-header-submenu__head-subtitle">
+
+                                                    ارسال رایگان بدون محدودیت قیمت، هدیه نقدی و بازگشت کالا تا ۳۰ روز پس از تحویل
+                                                </div>
+                                                <div>
+                                                    <ul class="sub-plas">
+                                                        <li style="color: #0fabc6;">
+                                                            <a> اطلاعات بیشتر و عضویت <i style="font-size:14px;color: #0fabc6;" class="fa  mr-2">&#xf104;</i></a>
+                                                        </li>
+                                                        <hr style="width: 90%;">
+                                                        <li>کالاهای دیجی پلاس</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+
+
+                                            <div class="drop-left">
+                                                <div class="img-digiplas">
+                                                    <a><img src="https://www.digikala.com/static/files/38492329.jpg"></a>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                   <div class="dropdown">
+                                    <a>دیجی کلاب</a>
+                                    <div class="sum-meno">
+                                        <div class="drop-right">
+                                            <div>
+                                                <div class=" logo-sub"><img src="https://www.digikala.com/static/files/1c93eb76.svg"></div>
+                                                <ul class="list-submeno">
+                                                    <li>
+
+                                                        <a>صفحه اصلی دیجی کلاب <i style="font-size:14px" class="fa text-muted mr-2">&#xf104;</i></a>
+                                                    </li>
+                                                    <li><a><i style="font-size:20px" class="fa text-muted ml-2">&#xf06b;</i>جوایز دیجی کلاب</a></li>
+                                                    <li>
+                                                        <a class="text-muted"> <i style="font-size:20px" class="fa ml-2">&#xf017;</i>تاریخچه امتیازات دیجی کلاب</a>
+                                                    </li>
+                                                    <li><a><i style="font-size:20px" class="fa text-muted ml-2">&#xf0e7;</i>ماموریت های دیجی کلابی</a></li>
+
+                                                    <hr class="mt-2 mb-0" style="width: 90%;">
+                                                    <li>
+                                                        <div class="timer-ghorekeshi">
+
+                                                            <a><i style="font-size:20px" class="fa text-muted ml-2">&#xf06b;</i>قرعه کشی  </a>
+                                                            <div id="demo-sunmeno"></div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                               </div>
+                                            <div class="drop-left">
+                                                <div>
+                                                    <a><img src="https://dkstatics-public.digikala.com/digiclub/53aa31f81138fd588df842086e152102979a00d7_1601458094.jpg"></a>
+                                                </div>
+                                            </div>
+                            
+                                    </div>
+                                </div>
+                            </li>
+                            <li><a>سوالی دارد</a></li>
+                            <li><a>فروشنده شوید</a></li>
+                    </ul>
+                </div>
+                <div class="headerleft">
+                    <ul class="place">
+                        <li>
+                            <span class="text-muted">  ارسال به خراسان شمالی، شیروان</span>
+                            <span class="text-muted"><i style="font-size:24px" class="fa">&#xf041;</i></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+<!-- //slider -->
+<div class="bg-slider">
+        <div class="cont">
+            <div class="row">
+                <div class="col-12 col-lg-8 col-sm-8 col-md-8">
+                    <div id="carouselExampleIndicators" class="carousel slide carousel-image" data-ride="carousel">
+                        <ol class="carousel-indicators circleslide">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/34e89b808da8d22d85cab948c4a68a85745dcbee_1618168208.jpg?x-oss-process=image/quality,q_80" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/f4dec19b6ce76cfcef533c797976ab6458d58446_1618655005.jpg?x-oss-process=image/quality,q_80" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/93c450a61b4348187372630713dae60f41856fb6_1618061633.jpg?x-oss-process=image/quality,q_80" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/add1294ad889261aaf8f330e411e80178a2d70ff_1617637271.jpg?x-oss-process=image/quality,q_80" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/7c8268a02bf58b7ea0ad9f32ccd009c0bdda2574_1618406676.jpg?x-oss-process=image/quality,q_80" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/4dec37edacaaf1cacc245635e20b156e09881eb5_1617784864.jpg?x-oss-process=image/quality,q_80" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/51b09054399f0c038ae571a8dcc1260fa566e47c_1618318152.jpg?x-oss-process=image/quality,q_80" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-4 col-sm-4 image-banner">
+                    <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/5fbbbc56b8e819201f8e20bfb37adc1d1cc1c383_1618728331.jpg?x-oss-process=image/quality,q_80" alt="">
+                    <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/e4bc782a30efd6f8f9ed0e7cea0a0cc8f7422c71_1618590543.jpg?x-oss-process=image/quality,q_80" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- //محصولات شگفت انگیز -->
+<!-- //4 تا باکس -->
+<div class="four-box">
+        <div class="container">
+            <div class="row banner2-item">
+                <div class="col-6 col-lg-3 col-xl-3 col-md-3   col-sm-3">
+                    <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/9438eecf3c585e7f2e2fa2e08c8aae7f58c05dc2_1617823661.jpg?x-oss-process=image/quality,q_80" alt="" width="100%">
+                </div>
+                <div class="col-6 col-lg-3 col-xl-3 col-md-3 col-sm-3">
+                    <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/84d7ea58e7bd00dacf34177145833f4310d63b6d_1618313447.jpg?x-oss-process=image/quality,q_80" alt="" width="100%">
+                </div>
+                <div class="col-6 col-lg-3 col-xl-3 col-md-3 col-sm-3">
+                    <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/2612f5570589bfbe03c7348f6e3a1f9d23604e57_1618597431.jpg?x-oss-process=image/quality,q_80" alt="" width="100%">
+                </div>
+                <div class="col-6 col-lg-3 col-xl-3 col-md-3 col-sm-3">
+                    <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/3a4a7914beaba2b57bdf3b386478a1225fd15858_1618597610.jpg?x-oss-process=image/quality,q_80" alt="" width="100%">
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
