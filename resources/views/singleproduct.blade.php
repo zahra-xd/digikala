0417485ec3336d23f28f9db23c52c48a44c447a0
zahra farokhi <z.farokhi63@gmail.com>
2021-05-01 16:05:18 +0430

    slide

--- resources/views/singleproduct.blade.php
+++ resources/views/singleproduct.blade.php
@@ -6,9 +6,9 @@
         <title>صفحه  محصولات </title>
      
          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-         {{-- <link rel="stylesheet" href="/css/style.css" > --}}
+         <link href="/public/font/shabnam-font-v1.1.0">
          <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-         {{-- <link rel="stylesheet" href="/css/stylepage2.css" > --}}
+         
          <link href="{{ asset('css/stylepage2.css') }}" rel="stylesheet">
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
